ff4cd1e18020c6ab795150d349936a91bdf03ae0
Matheus Melo <matheusprestesdmelo744@gmail.com>
2022-04-18 21:30:29 -0300

    resume desktop

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,8 @@
       />
 
       <Resume
+        btnValue="Learn More"
+
         imgA="https://www.elegantthemes.com/preview/Divi/wp-content/uploads/2018/01/finance-illustration-02.png"
         imgB="https://www.elegantthemes.com/preview/Divi/wp-content/uploads/2018/01/finance-illustration-03.png"
         imgC="https://www.elegantthemes.com/preview/Divi/wp-content/uploads/2018/01/finance-illustration-01.png"
@@ -40,6 +42,18 @@
         littleTitleA="teste1"
         littleTitleB="teste2"
         littleTitleC="teste3"
+
+        mainTitleA="Sending Money Home?"
+        mainTitleB="24 hrs, 7 Days a Week"
+        mainTitleC="Get Paid Easily & Quickly"
+
+        mainParagraphA="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud"
+        mainParagraphB="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud"
+        mainParagraphC="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud"
+
+        btnRedirectA="https://www.google.com"
+        btnRedirectB="https://www.google.com"
+        btnRedirectC="https://www.google.com"
       />
   </div>
 </template>
@@ -49,6 +63,7 @@ import Header from "./components/Header.vue";
 import Finances from "./components/Finances.vue";
 import SendingPayment from "./components/sendingPayment.vue";
 import GettingPaid from "./components/GettingPaid.vue";
+import Resume from "./components/Resume.vue";
 
 export default {
   name: 'App',
@@ -56,7 +71,8 @@ export default {
     Header,
     Finances,
     SendingPayment,
-    GettingPaid
+    GettingPaid,
+    Resume
 }
 }
 </script>
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,18 +1,80 @@
 <template>
   <div class="container">
-    <div class="row d-flex justify-content-center">
-      <div class="content col-md-4">
+    <div class="mt-5 pt-5 mb-5 pb-5">
+      <div class="content row mt-5">
         
-        <h1>teste</h1>
-
-        <div v-for="resumeItem in resumeItems" :key="resumeItem.id" class="d-flex flex-column">
-          <div class="mainImg">
-            <img :src="resumeItem.img" class="img-fluid">
-          </div>
-          <div class="littleTitle">
-            <p>{{resumeItem.littleTitle}}</p>
+        <!-- Content 1 --->
+        <div data-animation="right" class="col-md-4 container">
+          <div class="d-flex flex-column">
+            <div class="">
+              <div class="imgContent1 d-flex justify-content-center">
+                <img :src="imgA" class="img-fluid">
+              </div>
+              <div class="littleTitle pt-3">
+                <p class="text-center">{{littleTitleA}}</p>
+              </div>
+              <div class="mainHeader">
+                <h1 class="text-center">{{mainTitleA}}</h1>
+              </div>
+              <div class="mainParagraph">
+                <p class="text-center">{{mainParagraphA}}</p>
+              </div>
+              <div class="d-flex justify-content-center mainBtn">
+                <button>{{btnValue}}</button>
+              </div>
+            </div>
           </div>
         </div>
+        <!-- End Content 1 -->
+
+        <!-- Content 2 --->
+        <div data-animation="up" class="col-md-4 container">
+          <div class="d-flex flex-column">
+            <div class="">
+              <div class="imgContent1 d-flex justify-content-center">
+                <img :src="imgB" class="img-fluid">
+              </div>
+              <div class="littleTitle pt-3">
+                <p class="text-center">{{littleTitleB}}</p>
+              </div>
+              <div class="mainHeader">
+                <h1 class="text-center">{{mainTitleB}}</h1>
+              </div>
+              <div class="mainParagraph">
+                <p class="text-center">{{mainParagraphB}}</p>
+              </div>
+              <div class="d-flex justify-content-center mainBtn">
+                <button>{{btnValue}}</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End Content 2 -->
+
+        <!-- Content 3 --->
+        <div data-animation="left" class="col-md-4 container">
+          <div class="d-flex flex-column">
+            <div class="">
+              <div class="imgContent1 d-flex justify-content-center">
+                <img :src="imgC" class="img-fluid">
+              </div>
+              <div class="littleTitle pt-3">
+                <p class="text-center">{{littleTitleC}}</p>
+              </div>
+              <div class="mainHeader">
+                <h1 class="text-center">{{mainTitleC}}</h1>
+              </div>
+              <div class="mainParagraph">
+                <p class="text-center">{{mainParagraphC}}</p>
+              </div>
+              <div class="d-flex justify-content-center mainBtn">
+                <button>{{btnValue}}</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End Content 3 -->
+        
       </div>
     </div>
   </div>
@@ -23,27 +85,97 @@ export default {
     name:'Resume',
 
     props:{
-      resumeItems: [
-        {imgA:String, littleTitleA:String, mainTitleA:String},
-        {imgB:String, littleTitleB:String, mainTitleB:String},
-        {imgC:String, littleTitleC:String, mainTitleC:String}
-      ]
+          btnValue:String,
+
+          imgA:String, 
+          littleTitleA:String, 
+          mainTitleA:String,
+          mainParagraphA:String,
+          btnRedirectA:String,
+       
+          imgB:String, 
+          littleTitleB:String, 
+          mainTitleB:String,
+          mainParagraphB:String,
+          btnRedirectB:String,
+
+          imgC:String, 
+          littleTitleC:String, 
+          mainTitleC:String,
+          mainParagraphC:String,
+          btnRedirectC:String
     },
+    mounted(){
+        const finance = document.querySelectorAll('[data-animation]');
+        const animationClass = 'animated';
 
-    data: () => {
-      // return{
-      //   resumeItems: [
-      //     {img:`${this.imgA}`, littleTitle:`${this.littleTitleA}`, mainTitle:'', mainParagraph:'', buttonValue:'', redirectBtn:`${this.redirectBtnA}`},
-      //     {img:`${this.imgB}`, littleTitle:`${this.littleTitleB}`, mainTitle:'', mainParagraph:'', buttonValue:'', redirectBtn:`${this.redirectBtnB}`},
-      //     {img:`${this.imgC}`, littleTitle:`${this.littleTitleC}`, mainTitle:'', mainParagraph:'', buttonValue:'', redirectBtn:`${this.redirectBtnC}`}
-      //   ]
-      // }
-    }
-
-    
+        function animeScroll() {
+          const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+          finance.forEach(function(element) {
+            if((windowTop) > element.offsetTop) {
+              element.classList.add(animationClass);
+            } else {
+              element.classList.remove(animationClass);
+            }
+            console.log(element)
+          })
+        }
+        window.addEventListener('scroll', ()=>{
+            animeScroll();
+        })
+    }   
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.mainBtn{
+    button{
+        width: 10vw;
+        padding: .4rem 1.4rem;
+        border: 5px solid #4DAAF2;
+        border-radius: 2rem;
+        box-shadow: #4DAAF2 0 0 20px;
+        color: white;
+        background-color: #4DAAF2;
+        transition: .5s ease-in-out all;
+        font-weight: 600;
+        &:hover{
+            background-color: white;
+            color: #4DAAF2;
+            animation: shadowAnimated 2s infinite ease-in-out;
+        }
+        @keyframes shadowAnimated {
+            0%{
+                box-shadow: #4DAAF2 0 0 20px;
+            }
+            50%{
+                box-shadow: #4DAAF2 0 0 10px;
+            }
+            100%{
+                box-shadow: #4DAAF2 0 0 20px;
+            }
+        }
+    }
+}
+
+[data-animation]{
+  opacity: 0;
+  transition: 1s;
+}
+
+[data-animation="up"]{
+  transform: translate3d(0, 100px, 0);
+}
+[data-animation="left"]{
+  transform: translate3d(100px, 0, 0);
+}
+[data-animation="right"]{
+  transform: translate3d(-100px, 0, 0);
+}
+[data-animation].animated{
+  opacity: 1;
+  transform: translate3d(0, 0, 0);
+}
 
 </style>
